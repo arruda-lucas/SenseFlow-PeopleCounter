bb3df32c8b74ab9fed0a6bead96d1f4d3be9fcdc
arruda-lucas <lucas_arruda@discente.ufg.br>
2025-05-02 22:24:27 -0300

    concertando gpu

--- src/models/yolo_model.py
+++ src/models/yolo_model.py
@@ -1,17 +1,21 @@
 import torch
 from ultralytics import YOLO
 
-# Para descobrir qual o dispositivo de processamento
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Usando dispositivo: {device}")
-
-
 class Yolov5m:
     def __init__(self, model_path="src/models/yolov5mu.pt"):
-        self.model = YOLO(model_path).to(device)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Usando dispositivo: {self.device}")
+        
+        self.model = YOLO(model_path).to(self.device)
         self.model.eval()
 
     def detect(self, frame):
-        results = self.model.predict(frame, classes=[0], conf=0.5, verbose=False)
+        if isinstance(frame, torch.Tensor):
+            frame = frame.to(self.device)
 
+        results = self.model.predict(frame, classes=[0], conf=0.5, verbose=False)
         return results[0]
+
+if __name__ == "__main__":
+    detector = Yolov5m()
+    print(f"Modelo está na GPU? {next(detector.model.parameters()).is_cuda}")
